1bdb7680d1d47b0d70a9c4096c4874d8d33f4b77
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-06-20 17:16:34 +0200

    Makefile added

--- checker/main.c
+++ checker/main.c
@@ -6,7 +6,7 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/20 09:41:51 by daviwel           #+#    #+#             */
-/*   Updated: 2016/06/20 16:46:59 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/06/20 17:16:01 by ddu-toit         ###   ########.fr       */
 /*   Updated: 2016/06/20 16:00:02 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -28,6 +28,7 @@ int	main(int argc, char **argv)
 	t_list	*link;
 	t_list	*temp;
 
+	error_check(argc, argv);
 	first = ft_lstnew((void *)42);
 	link = ft_lstnew((void *)5);
 	temp = ft_lstnew((void *)36);
@@ -38,15 +39,4 @@ int	main(int argc, char **argv)
 	ft_lstrotrev(&first);
 	ft_lstrotrev(&first);
 	print_list(first);
-}
-
-int	main(int argc, char **argv)
-{
-	char	*ins;
-
-	error_check(argc, argv);
-	while (get_next_instruction() != -1)
-	{
-	}
-	return (0);
 }

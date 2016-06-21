223b0fb75a3fad12b4cadd86df9ec8567993d408
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-06-21 13:28:29 +0200

    -a

---
+++ checker/validate.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/21 11:53:38 by daviwel           #+#    #+#             */
+/*   Updated: 2016/06/21 12:23:15 by daviwel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+int	validate(t_info *info)
+{
+	int		i;
+	t_list	*crawl;
+	
+	i = 0;
+	crawl = info->a;
+	while (crawl != NULL)
+	{
+		if (crawl->next != NULL)
+		{
+			if (*(int *)crawl->data >= *(int *)crawl->next->data)
+			{
+				ft_printf("KO\n");
+				return (1);
+			}
+		}
+		crawl = crawl->next;
+		i++;
+	}
+	if (i > info->max || info->elem_b != 0)
+		ft_printf("KO\n");
+	else
+		ft_printf("OK\n");
+	return (1);
+}

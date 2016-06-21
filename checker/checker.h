aa46e33c3daadd7c0793ed9cfd23cabca2d3cc18
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-06-21 11:38:15 +0200

    added steps

--- checker/checker.h
+++ checker/checker.h
@@ -6,7 +6,7 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/20 09:31:57 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/06/21 10:51:20 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/06/21 11:37:59 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ typedef struct	s_info
 	int				elem_b;
 	int				max;
 	unsigned int	in_count;
+	t_list			*steps;
+	int				elem_steps;
 }				t_info;
 
 int				get_next_instruction(t_info *info);

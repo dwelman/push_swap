4891cbd42d3c0b2c87efad4d11c093ebe70ed7e5
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-06-24 11:59:46 +0200

    a

--- push_swap/test_moves2.c
+++ push_swap/test_moves2.c
@@ -6,7 +6,7 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/24 07:55:08 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/06/24 09:55:24 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/06/24 10:36:37 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	test_rb(t_info *info)
 {
-	if (info->b)
+	if (info->b && ft_strcmp(info->diffs->last, "rrb") != 0)
 	{
 		ft_lstrot(&info->b, info->max);
 		info->diffs->rb = calc_total_sortdiff(info);
 		ft_lstrotrev(&info->b, info->max);
+		ft_strcpy(info->diffs->last, "rb");
 	}
 	else
 		info->diffs->rb = 2147483647;
@@ -26,11 +27,12 @@ void	test_rb(t_info *info)
 
 void	test_rrb(t_info *info)
 {
-	if (info->b)
+	if (info->b && ft_strcmp(info->diffs->last, "rb") != 0)
 	{
 		ft_lstrotrev(&info->b, info->max);
 		info->diffs->rrb = calc_total_sortdiff(info);
 		ft_lstrot(&info->b, info->max);
+		ft_strcpy(info->diffs->last, "rrb");
 	}
 	else
 		info->diffs->rrb = 2147483647;
@@ -38,13 +40,14 @@ void	test_rrb(t_info *info)
 
 void	test_rrr(t_info *info)
 {
-	if (info->a && info->b)
+	if (info->a && info->b && ft_strcmp(info->diffs->last, "rrr") != 0)
 	{
 		ft_lstrotrev(&info->b, info->max);
 		ft_lstrotrev(&info->a, info->max);
 		info->diffs->rrr = calc_total_sortdiff(info);
 		ft_lstrot(&info->b, info->max);
 		ft_lstrot(&info->a, info->max);
+		ft_strcpy(info->diffs->last, "rrr");
 	}
 	else
 		info->diffs->rrr = 2147483647;
@@ -52,13 +55,14 @@ void	test_rrr(t_info *info)
 
 void	test_rr(t_info *info)
 {
-	if (info->a && info->b)
+	if (info->a && info->b && ft_strcmp(info->diffs->last, "rr") != 0)
 	{
 		ft_lstrot(&info->b, info->max);
 		ft_lstrot(&info->a, info->max);
 		info->diffs->rr = calc_total_sortdiff(info);
 		ft_lstrotrev(&info->b, info->max);
 		ft_lstrotrev(&info->a, info->max);
+		ft_strcpy(info->diffs->last, "rr");
 	}
 	else
 		info->diffs->rr = 2147483647;
--- push_swap/test_moves3.c
+++ push_swap/test_moves3.c
@@ -6,7 +6,7 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/24 08:10:24 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/06/24 09:55:11 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/06/24 10:38:17 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	test_pa(t_info *info)
 {
-	if (info->a)
+	if (info->a && ft_strcmp(info->diffs->last, "pb") != 0)
 	{
 		ft_lstpushpop(&info->a, &info->b);
 		info->diffs->pa = calc_total_sortdiff(info);
 		ft_lstpushpop(&info->b, &info->a);
+		ft_strcpy(info->diffs->last, "pa");
 	}
 	else
 		info->diffs->pa = 2147483647;
@@ -26,11 +27,12 @@ void	test_pa(t_info *info)
 
 void	test_pb(t_info *info)
 {
-	if (info->b)
+	if (info->b && ft_strcmp(info->diffs->last, "pa") != 0)
 	{
 		ft_lstpushpop(&info->b, &info->a);
 		info->diffs->pb = calc_total_sortdiff(info);
 		ft_lstpushpop(&info->a, &info->b);
+		ft_strcpy(info->diffs->last, "pb");
 	}
 	else
 		info->diffs->pb = 2147483647;

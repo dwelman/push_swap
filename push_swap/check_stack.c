7b69ab882c91ae14bf47b5adf777bf75b59d8b78
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-06-24 13:10:36 +0200

    a

--- push_swap/check_stack.c
+++ push_swap/check_stack.c
@@ -6,11 +6,31 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/23 07:47:54 by daviwel           #+#    #+#             */
-/*   Updated: 2016/06/23 08:21:09 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/06/23 11:12:03 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../checker/checker.h"
+
+int	check_stack_desc(t_info *info)
+{
+	int		i;
+	t_list	*crawl;
+
+	i = 0;
+	crawl = info->a;
+	while (crawl != NULL)
+	{
+		if (crawl->next != NULL)
+		{
+			if (*(int *)crawl->data <= *(int *)crawl->next->data)
+				return (0);
+		}
+		crawl = crawl->next;
+		i++;
+	}
+	return (1);
+}
 
 int	check_stack(t_info *info)
 {
